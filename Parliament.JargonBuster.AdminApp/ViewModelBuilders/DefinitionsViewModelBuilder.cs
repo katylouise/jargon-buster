e45615f7e08c66384cd6734d88faab3855eecd71
APPLEYARD <appleyardr@parliament.uk>
2016-04-18 16:37:06 +0100

    Finished removing all unnecessary code

--- Parliament.JargonBuster.AdminApp/ViewModelBuilders/DefinitionsViewModelBuilder.cs
+++ Parliament.JargonBuster.AdminApp/ViewModelBuilders/DefinitionsViewModelBuilder.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using AdminApp.ViewModels;
 using Parliament.JargonBuster.Core.Domain;
 using Parliament.JargonBuster.Core.Services;
