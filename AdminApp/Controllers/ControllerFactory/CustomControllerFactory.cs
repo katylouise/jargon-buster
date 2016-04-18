2d643f93794dc0c68154eb2ae241077584c5b8fc
giusepped <giusdesan@gmail.com>
2016-04-18 11:00:13 +0100

    injected the service from core to get all definitions into our homecontroller

---
+++ AdminApp/Controllers/ControllerFactory/CustomControllerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Parliament.JargonBuster.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace AdminApp.Controllers.ControllerFactory
+{
+    public class CustomControllerFactory : IControllerFactory
+    {
+        public IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            IDefinitionsService definitionsService = new DefinitionsService();
+            var controller = new HomeController(definitionsService);
+            return controller;
+        }
+
+        public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+            return SessionStateBehavior.Default;
+        }
+
+        public void ReleaseController(IController controller)
+        {
+            IDisposable disposable = controller as IDisposable;
+            if (disposable != null)
+                disposable.Dispose();
+        }
+    }
+}
---
+++ AdminApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parliament.JargonBuster.Core.Services;
+
+namespace AdminApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDefinitionsService _definitionsService;
+
+        public HomeController(IDefinitionsService definitionsService)
+        {
+            _definitionsService = definitionsService;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
