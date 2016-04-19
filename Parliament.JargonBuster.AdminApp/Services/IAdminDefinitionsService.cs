fcfb20a2f5a9d7435712ec4297a06a924da1fc9f
APPLEYARD <appleyardr@parliament.uk>
2016-04-19 10:14:51 +0100

    Refactored code into new service

---
+++ Parliament.JargonBuster.AdminApp/Services/IAdminDefinitionsService.cs
@@ -0,0 +1,15 @@
+﻿using AdminApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminApp.Services
+{
+    public interface IAdminDefinitionsService
+    {
+        void UpdateDefinitionViewModel(DefinitionViewModel definitionViewModel);
+        void AddDefinitionViewModel(DefinitionViewModel definitionViewModel);
+    }
+}
--- Parliament.JargonBuster.AdminApp/ViewModelBuilders/IDefinitionsViewModelBuilder.cs
+++ Parliament.JargonBuster.AdminApp/ViewModelBuilders/IDefinitionsViewModelBuilder.cs
@@ -14,8 +14,5 @@ namespace AdminApp.ViewModelBuilders
         DefinitionViewModel BuildDefinitionViewModelFromId(int id);
 
         DefinitionViewModel BuildDefinitionViewModelFromPhrase(string phrase);
-
-        void UpdateDefinitionViewModel(DefinitionViewModel definitionViewModel);
-        void AddDefinitionViewModel(DefinitionViewModel definitionViewModel);
     }
 }
