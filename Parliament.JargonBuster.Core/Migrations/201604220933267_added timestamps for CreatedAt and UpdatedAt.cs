4893093c85af048c8f33ce0647dde388b99325b3
giusepped <giusdesan@gmail.com>
2016-04-22 11:00:41 +0100

    added timestamps

---
+++ Parliament.JargonBuster.Core/Migrations/201604220933267_added timestamps for CreatedAt and UpdatedAt.cs
@@ -0,0 +1,16 @@
+namespace Parliament.JargonBuster.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedtimestampsforCreatedAtandUpdatedAt : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
