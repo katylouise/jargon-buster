3bf60e4e31bafff54a993ddbbb6be9344edf1ce9
APPLEYARD <appleyardr@parliament.uk>
2016-04-26 16:37:40 +0100

    All bugs should now be hopefully fixed

--- Parliament.JargonBuster.AdminApp/Scripts/admin-app/admin-app.js
+++ Parliament.JargonBuster.AdminApp/Scripts/admin-app/admin-app.js
@@ -2,13 +2,12 @@
 
 $(document).ready(function () {
 
-    $(".edit-alternatives__add").click(function () {
+    $("body").on("click", ".addAlternate", function () {
         var listId = $("li").last().attr("id");
         var counter = listId === undefined ? 0 : parseInt(listId) + 1;
         var newLi = '<li><input name="Alternates[' + counter + '].AlternateDefinition" type="text" value="" class="edit-alternatives__term"></li>';
         $("ul").append(newLi);
         $("li").last().attr("id", counter);
-        console.log("banana");
     });
 
     $('body').on('click', '.open-modal', function (e) {
@@ -17,8 +16,8 @@ $(document).ready(function () {
         $(this).attr('data-toggle', 'modal');
     });
 
-    $("#modal-container").on("hidden-bs-modal", function () {
-        $(this).removeData("bs-modal");
+    $("#modal-container").on("hidden.bs.modal", function () {
+        $(this).removeData("bs.modal");
     });
 });
 
