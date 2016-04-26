dbcd02dbd9bea4a38dfbafcc18647f0e8006c606
giusepped <giusdesan@gmail.com>
2016-04-26 11:00:48 +0100

    wip

--- Parliament.JargonBuster.AdminApp/Scripts/admin-app/admin-app.js
+++ Parliament.JargonBuster.AdminApp/Scripts/admin-app/admin-app.js
@@ -4,31 +4,27 @@ $(document).ready(function () {
 
     $(".addAlternate").click(function () {
         var listId = $("li").last().attr("id");
-        console.log(listId);
         var counter = listId === undefined ? 0 : parseInt(listId) + 1;
-        var newLi = '<li><input name="Alternates['+ counter +'].AlternateDefinition" type="text" value=""></li>';
+        var newLi = '<li><input name="Alternates[' + counter + '].AlternateDefinition" type="text" value="" class="edit-alternatives__term"></li>';
         $("ul").append(newLi);
         $("li").last().attr("id", counter);
-    });
-
-    $("body").on("click", ".open-modal", function (e) {
         console.log("banana");
+    });
+
+    $('body').on('click', '.open-modal', function (e) {
         e.preventDefault();
-        $(this).attr("data-target", "#modal-container");
-        $(this).attr("data-toggle", "modal");
+        $(this).attr('data-target', '#modal-container');
+        $(this).attr('data-toggle', 'modal');
     });
 
-    $("body").on("click", ".modal-close-btn", function (e) {
-        $("#modal-container").modal("hide");
+    $('body').on('click', '.modal-close-btn', function () {
+        $('#modal-container').modal('hide');
     });
 
-    $("#modal-container").on("hidden-bs-modal", function () {
-        this.removeData("bs-modal");
+    $('#modal-container').on('hidden.bs.modal', function () {
+        console.log("elephant");
+        $(this).removeData('bs.modal');
     });
-
-    $("#btnSave").click(function () {
-        $("#modal-container").modal("hide");
-    })
 
 });
 
